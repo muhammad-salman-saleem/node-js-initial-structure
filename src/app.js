6a37b4bf1dcda42d57edd9f9390157b27f43a58a
zeeshanrafiqrana <zeeshanrafiqrana01@gmail.com>
2024-07-09 00:00:52 +0500

    basic stracture for node setup

--- src/app.js
+++ src/app.js
@@ -14,4 +14,11 @@ app.use(express.urlencoded({extended: true,limit: "16kb" }));
 app.use(express.static("public"))
 app.use(cookieParser());
 
+//routes
+import userRouter from './routes/user.routes.js'
+
+
+//routes declerations
+app.use("/api/v1/users", userRouter);
+
 export default app;
